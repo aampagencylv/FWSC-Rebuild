d45c0268aa8c4ec6911c95e6147167c6ea5d7980
Steve Edwards <steve@aamp.agency>
2026-07-03 15:49:19 -0700

    Remove unused useEffect imports

--- src/app/directory/page.tsx
+++ src/app/directory/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useMemo, useEffect } from 'react'
+import { useState, useMemo } from 'react'
 import Link from 'next/link'
 import dynamic from 'next/dynamic'
 import styles from './directory.module.css'
